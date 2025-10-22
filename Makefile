43d14c0b5a6f78a9196544554554da47fbb6c642
Pasha Mesh <pasha.mesh@gmail.com>
2025-10-22 19:37:40 +0300

    tests: added unit tests for call api

--- Makefile
+++ Makefile
@@ -2,8 +2,16 @@
 
 all: test
 
+.PHONY: shell sh
+shell sh:
+	docker compose run --rm shell sh
+
 .PHONY: test
 test:
+	docker compose run --rm shell ./vendor/bin/phpunit tests
+
+.PHONY: test-legacy
+test-legacy:
 	docker compose run --rm test
 
 .PHONY: build
---
+++ src/CoMagic/CallApiConfig.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types=1);
+
+namespace CoMagic;
+
+use InvalidArgumentException;
+
+final class CallApiConfig
+{
+    private ?string $login;
+    private ?string $password;
+    private ?string $accessToken;
+
+    private string $entryPoint;
+
+    public function __construct(
+        ?string $login = null,
+        ?string $password = null,
+        ?string $accessToken = null,
+        ?string $entryPoint = null
+    ) {
+        if (empty($login) && empty($password) && empty($accessToken)) {
+            throw new InvalidArgumentException(
+                'Access token and/or login+password are required'
+            );
+        }
+
+        if (empty($login) xor empty($password)) {
+            throw new InvalidArgumentException(
+                'Login and password cannot be empty'
+            );
+        }
+
+        $this->login = $login;
+        $this->password = $password;
+        $this->accessToken = $accessToken;
+        $this->entryPoint = $entryPoint ?: 'https://callapi.comagic.ru/';
+    }
+
+    public function getLogin(): ?string
+    {
+        return $this->login;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function getAccessToken(): ?string
+    {
+        return $this->accessToken;
+    }
+
+    public function getEntryPoint(): ?string
+    {
+        return $this->entryPoint;
+    }
+}
